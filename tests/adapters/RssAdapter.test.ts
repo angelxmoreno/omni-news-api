f21b41b85a1eb863b070ce1214c8ae2862cf7980
Angel S. Moreno <angelxmoreno@gmail.com>
2025-08-21 22:55:21 -0400

    refactor: improved axios mock in RssAdapter tests

--- tests/adapters/RssAdapter.test.ts
+++ tests/adapters/RssAdapter.test.ts
@@ -1,9 +1,12 @@
-import { describe, expect, it } from 'bun:test';
-import { RssAdapter } from '../../src/adapters/RssAdapter.ts';
-import { createHttpClient } from '../../src/utils/createHttpClient.ts';
-import { mockHttpClient } from '../helpers/mockHttpClient.ts';
+import { afterEach, beforeEach, describe, expect, it } from 'bun:test';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { createHttpClient, RssAdapter } from '../../src';
 
 describe('RssAdapter', () => {
+    let mockAxios: MockAdapter;
+    let httpClient: ReturnType<typeof createHttpClient>;
+
     const rssUrl = '/rss';
     const rssXmlString = `
       <rss version="2.0">
@@ -20,14 +23,24 @@ describe('RssAdapter', () => {
         </channel>
       </rss>
     `;
-    mockHttpClient.onGet(rssUrl).reply(200, rssXmlString);
-    const httpClient = createHttpClient();
+
+    beforeEach(() => {
+        mockAxios = new MockAdapter(axios);
+        httpClient = createHttpClient();
+    });
+
+    afterEach(() => {
+        mockAxios.restore();
+    });
 
     it('fetches from the rss url provided', async () => {
+        mockAxios.onGet(rssUrl).reply(200, rssXmlString);
+
         const adapter = new RssAdapter({
             rssUrl,
             httpClient,
         });
+
         const data = await adapter.getArticles();
         expect(data.length).toBe(2);
         expect(data[0]?.title).toBe('Cats take over city');
--- tests/helpers/mockHttpClient.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import AxiosMockAdapter from 'axios-mock-adapter';
-
-// This sets the mock adapter on the default instance
-
-export const mockHttpClient = new AxiosMockAdapter(axios);
